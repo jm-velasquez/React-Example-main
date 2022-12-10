f564df75de8dc8e4daf360b7a5200d8579ae0b27
jm-velasquez <115376537+jm-velasquez@users.noreply.github.com>
2022-12-10 14:07:25 -0300

    Version funcional

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Contact from "../../pages/Contact/Contact";
-import Notfound from "../../pages/404/Notfound";
-import Home from "../../pages/Home/Home";
 import Header from "../header/Header";
+import Contact from "../../views/Contact/Contact";
+import Pokemones from "../../views/Pokemones/Poke";
+import RickMorty from "../../views/RickMorty/RM";
+import Art from "../../views/Art/Art.1";
+import Notfound from "../../views/404/Notfound";
+import Home from "../../views/Home/Home";
+
+import Footer from "../footer/Footer";
 
 export default function Main() {
   return (
@@ -12,9 +16,13 @@ export default function Main() {
       <Header />
       <Routes>
         <Route path="/contacto" element={<Contact />} />
+        <Route path="/poke" element={<Pokemones />} />
+        <Route path="/randm" element={<RickMorty />} />
+        <Route path="/art" element={<Art />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
